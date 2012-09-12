3abe67049f653287f98d8613ef6b5e1bfc6b1059
Charlie Gaines <beaugaines@gmail.com>
2012-09-12 17:26:31 -0400

    refactored and added new sounds to mark the end of a pomodoro block

--- buddhadoro.py
+++ buddhadoro.py
@@ -9,7 +9,10 @@ import time
 import subprocess
 
 # Sound constants
-ALARM = "tibetan_bowl.ogg"
+
+BOWL = "tibetan_bowl.ogg"
+BELL1 = "lutine_bell_1.ogg"
+BELL2 = "lutine_bell_2.ogg"
 DEV_NULL = open("/dev/null", "w")
 
 # fcns
@@ -27,20 +30,23 @@ def main(args):
   trest = int(trest)
 
   # begin work session
-  print "Beginning work session."
-  print "You're going to do %s work sessions of %s minutes length,\
-  with %s minute rests in between" % (work_sessions, twork, trest)
+  print "Welcome to Buddhadoro!"
+  print "You're going to do %s work sessions of %s minutes duration,\
+ with %s minute rests in between." % (work_sessions, twork, trest)
   while a < work_sessions:
+    alarm(BOWL)
     time.sleep(twork * 60)
-    alarm(ALARM)
-    time.sleep(trest * 60)
     a += 1
-    alarm(ALARM)
+    alarm(BOWL)
+    time.sleep(trest * 60)
+    print "You have completed %d work sessions." % a
 
   # Long rest session
-  print('Time for your long rest.')
+  alarm(BELL1)
+  print "You completed all %d of your work sessions.\
+  Time for your long rest." % a
   time.sleep(15 * 60)
-  alarm(ALARM)
+  alarm(BELL2)
   print('Well done!  You worked!')
 
 if __name__=="__main__":
